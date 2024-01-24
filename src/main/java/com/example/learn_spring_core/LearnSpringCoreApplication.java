5f97873d2fc9cb63a9bb0b2d7313db93cd234bd8
sealivanov <sealivanov@phoenixit.ru>
2024-01-24 21:33:26 +0300

    add prometheus

--- src/main/java/com/example/learn_spring_core/LearnSpringCoreApplication.java
+++ src/main/java/com/example/learn_spring_core/LearnSpringCoreApplication.java
@@ -2,7 +2,9 @@ package com.example.learn_spring_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class LearnSpringCoreApplication {
     public static void main(String[] args) {
